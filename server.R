990f9c174368e562d107b6fac29acf0eb04193f9
vaibhav sahu <vaibhav.sahu@dragonflydf.com>
2015-06-05 18:56:19 +0530

    Added Anova

--- server.R
+++ server.R
@@ -12,7 +12,7 @@ library("car")
 library("xtable")
 library("perturb")
 library("MASS")
-
+library("ggplot2")
 
 function(input,output) {
   
@@ -93,16 +93,351 @@ function(input,output) {
   #=================Hypothesis Testing for 2 populations ends===================#
   
   
+  #=================Anova Starts===================#
+  data2 = reactive({
+    data = data()
+    var = input$variable
+    return(data$var)
+  })
   
-  output$MenuItem = renderMenu({
-    menuItem("Menu Item", icon = icon("calendar"))
+  data = reactive({
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
+    else 
+      data = read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
+    return(data)
+  })
+  filterCatVar = reactive({
+    data = data()
+    data.temp = data
+    data.temp[colnames(data)] = lapply(data[colnames(data)],as.factor)
+    levels.temp = lapply(data.temp[colnames(data.temp)],levels)
+    length.levels = lapply(levels.temp,length)
+    length.levels = length.levels[length.levels<30]
+    return(names(length.levels))
+  })
+  
+  
+  
+  output$filename = renderText({
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
+    paste("File Name : ",inFile$name, "         Size:" , inFile$size/1000, "KB",sep = " ")
+  })
+  
+  output$contents <- renderTable({
+    if (is.null(data()))
+      return(NULL)
+    #data = read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
+    #stat.desc(data(),norm = T)
+    describe(data())
+    #describe(read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote))
+  })
+  
+  output$variable = renderUI({
+    #inFile <- input$file1
+    if (is.null(data()))
+      return(NULL)
+    variables = colnames(data())
+    checkboxGroupInput("variable", "Choose Variables for Plots", variables,selected = variables[[1]])
+  })
+  
+  output$plots <- renderUI({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$variable))
+      return(NULL)
+    plot_output_list_anova <- lapply(1:length(input$variable), function(i) {
+      #plotname <- paste(input$variable[[i]], i, sep="")
+      plotname <- paste("plot", i, sep="")
+      plotOutput(plotname, height = 350, width = 700)
+    })
+    
+    # Convert the list to a tagList - this is necessary for the list of items
+    # to display properly.
+    do.call(tagList, plot_output_list_anova)
     
   })
-  output$ProgressBox = renderInfoBox({
-    infoBox("Progress", paste0(25+input$Count,"%"),icon = icon("list"),color = "purple")
+  
+  
+  observe({ for (i in 1:length(input$variable)) {
+    
+    local({
+      my_i <- i
+      plotname <- paste("plot",my_i, sep="")
+      data = data() 
+      output[[plotname]] <- renderPlot({
+        par(oma = c( 2, 2, 2, 2 ) )
+        nf<- layout( matrix( c( 1, 2 ), 1, 2, byrow = TRUE),c(2, 2), c( 5, 5 ), TRUE )
+        layout.show(nf )
+        x = input$variable[[my_i]]
+        print(x)
+        ggplot(data= data,aes_string(x=input$variable[[my_i]]))+geom_histogram(aes(y=..density..),color="black",fill="white")+geom_density(alpha=0.2,fill="red")
+#         par(mfrow = c(1,2))
+#          hx <- dnorm(data[[input$variable[[my_i]]]],mean(data[[input$variable[[my_i]]]]),sd(data[[input$variable[[my_i]]]])/sqrt(length(data[[input$variable[[my_i]]]])))
+#          boxplot(data[[input$variable[[my_i]]]],col = "red",xlab = NULL)
+#          hist(data[[input$variable[[my_i]]]], col = "red",main = NULL,prob = T,xlab = NULL)
+#          curve(dnorm(x,mean(data[[input$variable[[my_i]]]]),sd(data[[input$variable[[my_i]]]])),add = T, col = "green", lwd = 2)
+# #         ##  Create an overall title.
+#          mtext( paste(input$variable[[my_i]]), outer = TRUE,cex = 1.5,col = "Blue")
+        #hist(data[[input$variable[[my_i]]]])
+      })
+    })
+  }
   })
   
-  output$approvalBox = renderInfoBox({
-    infoBox("Approval", "80%",icon = icon("thumbs-up",lib = "glyphicon"),color = "yellow")
+  output$varAnova = renderUI({
+    if (is.null(data()))
+      return(NULL)
+    variables = colnames(data())
+    catVar = filterCatVar()
+    #checkboxGroupInput("variable", "Choose Variables", variables,selected = variables[[1]])
+    switch(input$TypeAnova,
+           "anova" = div(class = "row-fluid",
+                         div(class="span5",selectInput("DepVar", "Dependent Variable",setdiff(variables,catVar))),
+                         div(class="span5",checkboxGroupInput("IndepVar", "Independent Variables", catVar,selected = catVar[1]))
+           ),
+           #                "ancova" = div(class = "row-fluid",
+           #                               div(class="span5",selectInput("DepVar", "Dependent Variable",variables)),
+           #                               div(class="span5",checkboxGroupInput("IndepVar", "Independent Variables", catVar,selected = catVar[1]))
+           #                ),
+           "manova" =  div(class = "row-fluid",
+                           div(class="span5",checkboxGroupInput("MDepVar", "Dependent Variables", setdiff(variables,catVar),selected = variables[[1]])),
+                           div(class="span5",checkboxGroupInput("MIndepVar", "Independent Variables", catVar,selected = catVar[1]))
+           ) )
+    
   })
+
+output$anovaPlot = renderPlot({
+  data=data()
+  if (is.null(input$IndepVar))
+    return(NULL)
+  if (is.null(data()))
+    return(NULL)
+  if (length(input$IndepVar)==1)
+        return(qplot(data=data,x = factor(data[[input$IndepVar]]),y=data[[input$DepVar]],geom=c("boxplot","jitter"),xlab=input$IndepVar,ylab=input$DepVar,fill=factor(data[[input$IndepVar]]))
+               +theme(axis.title=element_text(face="bold",size="12", color="brown"), legend.position="top")+scale_fill_discrete(name = input$IndepVar)) 
+#   if (length(input$IndepVar)==2)
+#         return(qplot(data=data,x = factor(data[[input$IndepVar[[1]]]]),y=data[[input$DepVar]],geom=c("boxplot","jitter"),xlab=input$IndepVar,ylab=input$DepVar,fill=factor(data[[input$IndepVar[1]]]))
+#                 +theme(axis.title=element_text(face="bold",size="12", color="brown"),legend.position="top")+facet_wrap(.~factor(data[[input$IndepVar[[2]]]])))
+})
+
+
+output$anovaPlotText = renderText({
+  if (is.null(input$IndepVar))
+    return(NULL)
+  if (is.null(data()))
+    return(NULL)
+  if(length(levels(factor(input$IndepVar)))>15)
+    paste("Independent Variable is not Categorical or has more than 15 categories")
+})
+  
+  output$textAnova1 = renderText({
+    paste("Response Variable:",input$DepVar,sep = " ")
+  })
+  output$textAnova2 = renderText({
+    paste("Independent Variables:",paste(input$IndepVar,collapse = ","),sep = " ")
+  })
+  
+  anova.fit = reactive({
+    if (is.null(input$IndepVar))
+      return(NULL)
+    data1 = data()
+    data1[input$IndepVar] = lapply(data1[input$IndepVar],as.factor)
+    data1[input$DepVar] = lapply(data1[input$DepVar],as.numeric)
+    e1 = paste(input$DepVar,paste(input$IndepVar,collapse = "*"),sep = "~")
+    fit = aov(formula(e1),data = data1)
+    return(fit)
+    
+  }
+  )
+  output$SummaryAnova <- renderTable({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$IndepVar))
+      return(NULL)
+    
+    summary(anova.fit())
+  })
+  
+  anova.plot = reactive({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$IndepVar))
+      return(NULL)
+    layout(matrix(c(1,2,3,4),2,2))
+    plot(anova.fit())
+  })
+  
+  output$PlotAnova = renderPlot({
+    anova.plot()
+  })
+  
+  output$TukeyPlot <- renderPlot({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$IndepVar))
+      return(NULL)
+    
+    plot(TukeyHSD(anova.fit()))
+  })
+  
+  output$Tukey <- renderTable({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$IndepVar))
+      return(NULL)
+    
+    data.frame(TukeyHSD(anova.fit())[1])
+  })
+  
+  bartlett = reactive({
+    if (is.null(input$IndepVar))
+      return(NULL)
+    data1 = data()
+    data1[input$IndepVar] = lapply(data1[input$IndepVar],as.factor)
+    data1[input$DepVar] = lapply(data1[input$DepVar],as.numeric)
+    e1 = paste(input$DepVar,paste("interaction(",paste(input$IndepVar,collapse = ","),")",sep = ""),sep = "~")
+    b.fit = bartlett.test(formula(e1),data = data1)
+    data.bartlett = cbind(b.fit[1],b.fit[2],b.fit[3],b.fit[4],b.fit[5])
+    colnames(data.bartlett) = c("Bartlett's K-squared","df","p.value","data.name","method")
+    return(data.bartlett)
+    
+  })
+  output$Bartlett <- renderTable({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$IndepVar))
+      return(NULL)
+    
+    bartlett()
+  })
+  
+  lavene = reactive({
+    if (is.null(input$IndepVar))
+      return(NULL)
+    data1 = data()
+    data1[input$IndepVar] = lapply(data1[input$IndepVar],as.factor)
+    data1[input$DepVar] = lapply(data1[input$DepVar],as.numeric)
+    e1 = paste(input$DepVar,paste(input$IndepVar,collapse = "*"),sep = "~")
+    l.fit = leveneTest(formula(e1),data = data1)
+    data.lavene = cbind(l.fit[1],l.fit[2],l.fit[3])
+    colnames(data.lavene) = c("df","F value","Pr(>F)")
+    return(data.lavene)
+  })
+  
+  output$Levene <- renderTable({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$IndepVar))
+      return(NULL)
+    lavene()
+  })
+  
+  summary_manova = reactive({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$MIndepVar))
+      return(NULL)
+    if (is.null(input$MDepVar))
+      return(NULL)
+    data1 = data()
+    data1[input$MIndepVar] = lapply(data1[input$MIndepVar],as.factor)
+    data1[input$MDepVar] = lapply(data1[input$MDepVar],as.numeric)
+    e1 = paste(paste("cbind(",paste(input$MDepVar,collapse = ","),")",sep = ""),paste(input$MIndepVar,collapse = "*"),sep = "~")
+    fit = manova(formula(e1),data = data1)
+    summ.manova= summary.aov(fit)
+    tables <- list()
+    for (i in 1:length(summ.manova)){
+      tables[i] = paste(strong("Response Variable: "),input$MDepVar[i],br(),br(),print(xtable(summ.manova[1]),type = "html"),br(),sep = "" )
+    }
+    
+    return(lapply(tables, paste))
+    
+    
+  })
+  output$SummaryManova= renderUI({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$MIndepVar))
+      return(NULL)
+    if (is.null(input$MDepVar))
+      return(NULL)
+    #print(summary.aov(manova.fit()))
+    #   s1 = print(xtable(summary.aov(manova.fit())[1]),type = "html")
+    #   s3 = print("response Variable:")
+    #   s2 = print(xtable(summary.aov(manova.fit())[2]),type = "html")
+    out = summary_manova()
+    return(div(HTML(out),class="shiny-html-output"))
+    #HTML(print(xtable(summary.aov(manova.fit())[1]),type = "html"))
+    #HTML(print(xtable(summary.aov(manova.fit())[2]),type = "html"))
+    # HTML(print(xtable(manova.fit()),type = "html"))
+  })
+  
+  # output$fitManova= renderTable({
+  #   if (is.null(data()))
+  #     return(NULL)
+  #   manova.fit()
+  #   
+  # })
+  
+  manova.fit = reactive({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$MIndepVar))
+      return(NULL)
+    if (is.null(input$MDepVar))
+      return(NULL)
+    data1 = data()
+    data1[input$MIndepVar] = lapply(data1[input$MIndepVar],as.factor)
+    data1[input$MDepVar] = lapply(data1[input$MDepVar],as.numeric)
+    e1 = paste(paste("cbind(",paste(input$MDepVar,collapse = ","),")",sep = ""),paste(input$MIndepVar,collapse = "*"),sep = "~")
+    fit = manova(formula(e1),data = data1)
+    return(fit)
+  })
+  output$PillaiManova= renderTable({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$MIndepVar))
+      return(NULL)
+    if (is.null(input$MDepVar))
+      return(NULL)
+    #   out = print(xtable(summary(manova.fit(),test = "Pillai")),type= "html")
+    #   return(div(HTML(out),class="shiny-html-output"))
+    data.frame(summary(manova.fit(),test = "Pillai")[4])
+  })
+  output$WilksManova= renderTable({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$MIndepVar))
+      return(NULL)
+    if (is.null(input$MDepVar))
+      return(NULL)
+    data.frame(summary(manova.fit(),test = "Wilks")[4])
+  })
+  output$HotLawManova= renderTable({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$MIndepVar))
+      return(NULL)
+    if (is.null(input$MDepVar))
+      return(NULL)
+    data.frame(summary(manova.fit(),test = "Hotelling-Lawley")[4])
+  })
+  output$RoyManova= renderTable({
+    if (is.null(data()))
+      return(NULL)
+    if (is.null(input$MIndepVar))
+      return(NULL)
+    if (is.null(input$MDepVar))
+      return(NULL)
+    data.frame(summary(manova.fit(),test = "Roy")[4])
+  })
+  
+  
+  #=================Anova Ends===================#
+  
+ 
 }
--- ui.R
+++ ui.R
@@ -1,31 +1,16 @@
-header =   dashboardHeader(title = "Stats",
-                           dropdownMenu(type = "messages",
-                                        messageItem(
-                                          from = "sales-dept",message = "sales ready for this month"),
-                                        messageItem(
-                                          from ="new user", message = "Register", time = "12:45", icon = icon("question")),
-                                        messageItem(
-                                          from= "support", message = "how can i help u?", icon = icon("life-ring"), time = "13-12-2015")
-                                        
-                           ))
-
+header =   dashboardHeader(title = "Statistics")
 
 sidebar = dashboardSidebar(
-  sidebarUserPanel("User Name", 
-                   subtitle = a(href = "#", icon("circle", class = "text-success"), "Online"),
-                   image = "C:/Users/Vaibhav/Desktop/Test/StatsDashBoard/fetchImage.jpg"),
   sidebarMenu(
-    menuItem("Confidence Interval", tabName = "tabCI",icon = icon("area-chart")),
-    menuItem("Hypothesis Testing", tabName = "tabHT", icon = icon("area-chart"),badgeLabel = "New",badgeColor = "green"),
-    menuItem("Hypothesis Testing for 2 Populations", tabName = "tabHT2",icon = icon("area-chart")),
-    menuItem("Anova", tabName = "tabAnova",icon = icon("area-chart")),
-    menuItem("Regression", tabName = "tabRegression",icon = icon("line-chart")),
-    menuItemOutput("MenuItem")
-    ),
-  sidebarSearchForm(textId = "searchText", label = "Search...", buttonId = "SearchButton")
-  
-  
-)
+              menuItem("Confidence Interval", tabName = "tabCI",icon = icon("area-chart")),
+              menuItem("Hypothesis Testing", tabName = "tabHT", icon = icon("area-chart"),badgeLabel = "New",badgeColor = "green"),
+              menuItem("Hypothesis Testing for 2 Populations", tabName = "tabHT2",icon = icon("area-chart")),
+              menuItem("Anova", tabName = "tabAnova",icon = icon("area-chart")),
+              menuItem("Regression", tabName = "tabRegression",icon = icon("line-chart"))
+              
+              
+              )
+  )
 
 body = dashboardBody(
   tabItems(
@@ -56,25 +41,25 @@ body = dashboardBody(
     tabItem(tabName = "tabHT",
             h2("Hypothesis Testing"),
             fluidRow(
-              box(title = "Parameter for Hypothesis Testing",solidHeader = T, collapsible =T, width=4,height=800,status = "primary",
-              h4("Hypothesis Testing for Population Mean"),
-              p("Select the type of your Hypothesis Test"),
-              radioButtons("type",label = "",choices = list("Two Tail" = 1, "Left Tail" = 2, "Right Tail" = 3),selected = 1),
-              numericInput("meanHT",label = h4("Population Mean(Mu)"),value = 0),
-              numericInput("SmeanHT",label = h4("Sample Mean(Xbar)"),value = 0),
-              radioButtons("sdchoice", label = h4("Standard Deviation"), choice = list("Population Standard Deviation(Sigma)" = 1, 
-                                                                              "Sample Standard Deviation(s)" = 2),selected = 1),
-              
-              numericInput("sdHT",label = "", value =1),
-              numericInput("alpha", label = h4("Alpha"), value = 0.05),
-              numericInput("n", label = h4("Sample Size(n)"), value = 100)
-              
-#               infoBox("New Orders", 10*2, fill =F),
-#               infoBoxOutput("ProgressBox"),
-#               infoBoxOutput("approvalBox"),
-#               box(width = 4, actionButton("Count","increment Progress"))
+              box(title = "Parameter for Hypothesis Testing",solidHeader = T, collapsible =T, width=4,status = "primary",
+                      h4("Hypothesis Testing for Population Mean"),
+                      p("Select the type of your Hypothesis Test"),
+                      radioButtons("type",label = "",choices = list("Two Tail" = 1, "Left Tail" = 2, "Right Tail" = 3),selected = 1),
+                      numericInput("meanHT",label = h4("Population Mean(Mu)"),value = 0),
+                      numericInput("SmeanHT",label = h4("Sample Mean(Xbar)"),value = 0),
+                      radioButtons("sdchoice", label = h4("Standard Deviation"), choice = list("Population Standard Deviation(Sigma)" = 1, 
+                                                                                      "Sample Standard Deviation(s)" = 2),selected = 1),
+                      
+                      numericInput("sdHT",label = "", value =1),
+                      numericInput("alpha", label = h4("Alpha"), value = 0.05),
+                      numericInput("n", label = h4("Sample Size(n)"), value = 100)
+                      
+        #               infoBox("New Orders", 10*2, fill =F),
+        #               infoBoxOutput("ProgressBox"),
+        #               infoBoxOutput("approvalBox"),
+        #               box(width = 4, actionButton("Count","increment Progress"))
               ),
-              box(title = "Hypothesis Testing", solidHeader = T, collapsible = T, status = "primary", width = 8, height = 800,
+              box(title = "Hypothesis Testing", solidHeader = T, collapsible = T, status = "primary", width = 8,
                   #div(p("Z-Test"),align="center", style = "font-size:150%;color:blue;"),
                   p(strong(span("Z-Test:",style = "color:blue")), "We are applying Z-test for hypotheis testing 
                        when population standard deviation is known."),
@@ -142,11 +127,8 @@ body = dashboardBody(
                            numericInput("nP", label = "n", value = 100.0),
                            numericInput("alphaP", label = "alpha", value = 0.05)
                   )
-                  
-              
               ),
-              
-          tabBox(title = "Hypothesis Testing for 2 populations", width = 7,side= "right",selected = strong(p("Paired Sample")),
+        tabBox(title = "Hypothesis Testing for 2 populations", width = 7,side= "right",selected = strong(p("Paired Sample")),
                  tabPanel(strong(p("Proportions")),h4("Proportions"),
                          plotOutput("plotProportions"),
                          textOutput("textProp1"),
@@ -162,14 +144,93 @@ body = dashboardBody(
                           textOutput("txtPaired1"),
                           textOutput("txtPaired2"),
                           textOutput("txtPaired3"))
-                 
-            
-            
             )
-      )
-
+      ) ),
 #=================Hypothesis Testing for 2 populations Ends====================#
- )
+
+#=============================ANOVA Starts=====================================#
+      tabItem(tabName= "tabAnova",h2("Analysis of Variance"),
+              fixedRow(
+                column(width= 3,
+                  box(title = strong("Input Parameters"),solidHeader =T,collapsible =T,width = NULL, status = "primary",collapsed=F,
+                       fileInput('file1',"",accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+                       div(class = "row-fluid",
+                                div(class="col-md-6",radioButtons("sep",h5("Separator"),c(Comma=',',Semicolon=';',Tab='\t'),',')),
+                                div(class="col-md-6",radioButtons("quote",h5("Quote"),c(None='','Double Quote'='"','Single Quote'="'"),'"'))
+                             ),
+                      checkboxInput('header', 'Header', TRUE)),
+                  box(title = strong("Input Variables for Anova"), solidHeader =T,collapsible =T,width = NULL, status = "primary",
+                    uiOutput("variable"),
+                          radioButtons('TypeAnova', 'Type of Anova',c(ANOVA='anova',MANOVA='manova'),'anova'),
+                          strong(h6(span("*** Do not use SAME Dependent and Independent Variables",style = "color:red"))),
+                          uiOutput("varAnova"))   
+                      ),
+                column(width=9,
+                  box(title = strong(p("Data Summary & Plots")),solidHeader =T, collapsible =T,width = NULL, status = "warning",
+                     tabBox(title = strong("Summary"),width = 14,side = "right",
+                            tabPanel(strong("Summary"),
+                     textOutput('filename'),
+                     tableOutput('contents'),
+                     strong(h5(div("Notes:",style = "color:red"))),
+                     p("For more Info on Data Summary refer",a("Link.",href = "https://r-forge.r-project.org/scm/viewvc.php/*checkout*/pkg/R/stat.desc.R?revision=2&root=pastecs&pathrev=4"))
+                     ),
+                         tabPanel(strong("Exploratory Plots"),uiOutput('plots'))
+                     ))
+                 
+              ),
+#               ),
+#                 
+#               fluidRow(
+                box(title = strong("Output"),solidHeader=T,status="warning",collapsible=T,width=9,collapsed=T,
+                    tabBox(title = strong("Analysis Of Variance"),width = 14,side = "left",
+                           tabPanel(strong("Anova"),
+                                    fluidRow(
+                                      column(width = 6,
+                                        box(title=strong("Box Plot"),textOutput("anovaPlotText"),width=NULL,solidHeader=T,collapsible=T,status="primary",
+                                            plotOutput("anovaPlot",width = "100%", height = "630px")),
+                                        box(title = strong("Tukeyplot"),plotOutput('TukeyPlot'),width =NULL,status="primary",collapsible=T,solidHeader=T),
+                                        box(title = strong(strong("Tukey HSD(Honestly Significant Difference)")),
+                                            solidHeader=T,collapsible=T,width=NULL,status="primary",tableOutput('Tukey'))),
+                                        column(width=6,
+                                               box(title = strong("Summary"),width=NULL,solidHeader=T,collapsible=T,status="primary",
+                                                   textOutput('textAnova1'),textOutput('textAnova2'),tableOutput('SummaryAnova')),
+                                               box(title = "Residual Plots",width = NULL,solidHeader=T,collapsible=T,status="primary",
+                                                    plotOutput('PlotAnova')),
+                                                box(title = strong("Tests"),width=NULL,solidHeader=T,collapsible=T,status="primary",
+                                                    strong(strong("Bartlett test of homogeneity of variances")),
+                                                    h6("***There must be at least 2 observations in each group"),
+                                                    tableOutput('Bartlett'),
+                                                    strong(strong("Levene's Test for Homogeneity of Variance")),
+                                                    h6("***Levene's test is not appropriate with quantitative explanatory variables"),
+                                                    tableOutput('Levene'))
+                                    )
+                                      )
+                                    ),
+                           tabPanel(
+                              strong("Manova"),
+                              fluidRow(box(title=strong("Summany Manova"),collapsible=T,solidHeader=T,status="primary",
+                                    uiOutput("SummaryManova")),
+                                    box(title=strong("Pillai's Test"),collapsible=T,solidHeader=T,status="primary",
+                                    tableOutput("PillaiManova")),
+                                    box(title=strong("Wilks's Test"),collapsible=T,solidHeader=T,status="primary",
+                                    tableOutput("WilksManova")),
+                                    box(title=strong("Hotelling-Lawley's Test"),collapsible=T,solidHeader=T,status="primary",
+                                    tableOutput("HotLawManova")),
+                                    box(title=strong("Roy's Test"),collapsible=T,solidHeader=T,status="primary",
+                                    tableOutput("RoyManova"))))
+                           )
+                ))
+      
+              ),
+
+#==============================ANOVA Ends======================================#
+
+#=================Regression Starts====================#
+      tabItem(tabName= "tabRegression",h2("Linear Regression Model") )
+
+
+
+#==============================Regression Ends=================================#
 ))
 
-dashboardPage(skin = "black",header,sidebar,body)
+dashboardPage(skin = "blue",header,sidebar,body)
